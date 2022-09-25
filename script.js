b228d07df718a43fdcef506acb03ef98080d2250
Shreshtha Indoriya <indoriyashreshtha@gmail.com>
2022-09-25 11:52:53 -0700

    initial commite

--- script.js
+++ script.js
@@ -1,59 +1,107 @@
+var a = 0;
+var b = 0;
+var c = 0;
+var op = "";
+function clearDisplay() {
+    document.getElementById("result").value = "";
+}
 
-var a=0;
-var b=0;
-var op=null;
-var c=0;
+function back() {
+    let str=document.getElementById("result").value;
+   let newstr=str.substring(0, str.length - 1);
+   document.getElementById("result").value=newstr;
+}
 
-
-function display(num){
-    var display=document.getElementById('result');
-    
-    if(display.value=="0"){
-        display.value=num;
-    }else{
-        display.value=display.value+num;
+function display(num) {
+    var display = document.getElementById("result");
+    if (display.value == 0) {
+        display.value = num;
+    } else {
+        display.value = display.value + num
+        
     }
     
 
 }
-function clearDisplay(){
-    document.getElementById('result').value="0";
-}
-function add(){
-    a=document.getElementById('result').value;
-    op='+';
-    document.getElementById('result').value="0";
-}
-function sub(){
-    a=document.getElementById('result').value;
-    op='-';
-    document.getElementById('result').value="0";
-}
-function multi(){
-    a=document.getElementById('result').value;
-    op='*';
-    document.getElementById('result').value="0";
-}
-function div(){
-    a=document.getElementById('result').value;
-    op='/';
-    document.getElementById('result').value="0";
-}
+function add() {
 
-function equal(){
-    b=document.getElementById('result').value;
-    if(op=="+"){
-        c=parseFloat(a)+parseFloat(b);
+    a = document.getElementById("result").value;
+    op = "+";
+    document.getElementById("result").value = 0;
+
+
+}
+function sub() {
+
+    a = document.getElementById("result").value;
+    op = "-";
+    document.getElementById("result").value = 0;
+
+
+}
+function multi() {
+
+    a = document.getElementById("result").value;
+    op = "*";
+    document.getElementById("result").value = 0;
+
+
+}
+function div() {
+
+    a = document.getElementById("result").value;
+    op = "/";
+    document.getElementById("result").value = 0
+
+
+}
+function mod() {
+
+    a = document.getElementById("result").value;
+    op = "%";
+    document.getElementById("result").value = 0
+
+
+}
+function equal() {
+    b = document.getElementById("result").value;
+    switch (op) {
+        case "+":
+            c = parseFloat(a)+parseFloat(b);;
+            document.getElementById("result").value = c;
+            break;
+        case "-":
+            c = parseFloat(a)-parseFloat(b);
+            document.getElementById("result").value = c;
+            break;
+        case "*":
+            c = parseFloat(a)*parseFloat(b);
+            document.getElementById("result").value = c;
+            break;
+
+        case "/":
+            c = parseFloat(a)/parseFloat(b);
+            document.getElementById("result").value = c;
+            break;
+            case "%":
+                c = parseFloat(a)%parseFloat(b);
+                document.getElementById("result").value = c;
+                break;
     }
-    if(op=="-"){
-        c=parseFloat(a)-parseFloat(b);
-    }
-    if(op=="*"){
-        c=parseFloat(a)*parseFloat(b);
-    }
-    if(op=="/"){
-        c=parseFloat(a)/parseFloat(b);
-    }
-    console.log(a,b,c,op);
-    document.getElementById('result').value=c;
+    // if(op=="+"){
+    //     c=parseFloat(a)+parseFloat(b);
+    //     document.getElementById("result").value=c;
+    // }
+    // if(op=="-"){
+    //     c=parseFloat(a)-parseFloat(b);
+    //     document.getElementById("result").value=c;
+    // }
+    // if(op=="*"){
+    //     c=parseFloat(a)*parseFloat(b);
+    //     document.getElementById("result").value=c;
+    // }
+    // if(op=="/"){
+    //     c=parseFloat(a)/parseFloat(b);
+    //     document.getElementById("result").value=c;
+    // }
 }
